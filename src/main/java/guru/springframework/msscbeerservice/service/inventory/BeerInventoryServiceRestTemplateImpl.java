9b22d29bbeb9efc915cd3dcf793f8d8b42168975
Marc <marc.marechal@dell.com>
2021-05-12 10:07:55 +0800

    Open Feign Client to retrieve Beer Inventory

--- src/main/java/guru/springframework/msscbeerservice/service/inventory/BeerInventoryServiceRestTemplateImpl.java
+++ src/main/java/guru/springframework/msscbeerservice/service/inventory/BeerInventoryServiceRestTemplateImpl.java
@@ -4,6 +4,7 @@ import guru.springframework.msscbeerservice.service.inventory.model.BeerInventor
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.context.annotation.Profile;
 import org.springframework.core.ParameterizedTypeReference;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.ResponseEntity;
@@ -14,12 +15,13 @@ import java.util.List;
 import java.util.Objects;
 import java.util.UUID;
 
-@Service
 @Slf4j
+@Profile("!local-discovery")
+@Service
 @ConfigurationProperties(prefix="sfg.brewery", ignoreUnknownFields = false)
 public class BeerInventoryServiceRestTemplateImpl implements BeerInventoryService {
 
-    private final String INVENTORY_PATH = "/api/v1/beer/{beerId}/inventory";
+    public static final String INVENTORY_PATH = "/api/v1/beer/{beerId}/inventory";
     private final RestTemplate restTemplate;
 
     private String beerInventoryServiceHost;
---
+++ src/main/java/guru/springframework/msscbeerservice/service/inventory/InventoryServiceFeignClient.java
@@ -0,0 +1,20 @@
+package guru.springframework.msscbeerservice.service.inventory;
+
+import guru.springframework.msscbeerservice.service.inventory.model.BeerInventoryDto;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+import java.util.List;
+import java.util.UUID;
+
+/**
+ * Created by marecm on 5/12/2021
+ */
+@FeignClient("inventory-service")
+public interface InventoryServiceFeignClient {
+
+    @GetMapping(value = BeerInventoryServiceRestTemplateImpl.INVENTORY_PATH)
+    ResponseEntity<List<BeerInventoryDto>> getOnHandInventory(@PathVariable UUID beerId);
+}
